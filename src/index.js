8fb81d665afe5f811b87f74eae49c7521814d47f
candle <ted@wicrep.com>
2017-11-10 10:13:49 +0900

    Create actions import script

--- src/index.js
+++ src/index.js
@@ -1,4 +1,47 @@
+import globby from 'globby'
+import {
+  getImportPath,
+  template
+} from 's2s-utils'
+
+const builders = {
+  redux: template(`import { createAction } from 'redux-actions'`)
+}
+
 module.exports = (babel) => {
-  console.log('plugin enabled')
-  return {}
+  var t = babel.types;
+
+  const defaultExport = (source) => t.ExportAllDeclaration( t.stringLiteral(source))
+
+  return {
+    name: "s2s-redux-actions-root",
+    visitor: {
+      Program: {
+        exit(path, state) {
+	        const { input, output } = state.opts
+          if (!input) {
+            throw new Error('require input option')
+          }
+
+          if (!output) {
+            throw new Error('require output option')
+          }
+
+          const files = globby.sync(input)
+          const index = files.indexOf(output)
+
+          if (index > -1) {
+            files.splice(index, 1);
+          }
+
+          const imports = files.map(f => defaultExport(getImportPath(output, f)))
+
+          path.node.body = [
+            builders.redux({}),
+            ...imports
+          ]
+        }
+      }
+    }
+  }
 }
